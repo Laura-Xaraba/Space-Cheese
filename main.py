2bd31d01264dcced6e8eaedef4ba6d5123c49eee
Laura Xaraba <lauraxaraba@gmail.com>
2025-07-03 13:42:14 -0300

    paths config

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ COR_PRETO = (0, 0, 0)
 
 pygame.init()
 pygame.mixer.init()
+
+# Configuração dos paths
+diretorio_principal = os.path.dirname(__file__)
+diretorio_img = os.path.join(diretorio_principal, 'assets', 'img', 'sfx')
+
