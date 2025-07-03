e608946e577f0444361ae07490429fc84bd0b71b
Laura Xaraba <lauraxaraba@gmail.com>
2025-07-03 13:42:48 -0300

    class stablishing - PLAYER

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ pygame.mixer.init()
 diretorio_principal = os.path.dirname(__file__)
 diretorio_img = os.path.join(diretorio_principal, 'assets', 'img', 'sfx')
 
+# Estabelecimento de classes
+class Jogador(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        # Carregamento da imagem, Ajuste do tamanho e posição inicial
+        self.image = pygame.image.load(os.path.join(diretorio_img, 'player__space_ship.png')).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 40))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = LARGURA_TELA / 2
+        self.rect.bottom = ALTURA_TELA - 10
+        self.speedx = 0
