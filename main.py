2a9bf5d98ca096f2a5980d66a5bae6c2ad58ad36
Laura Xaraba <lauraxaraba@gmail.com>
2025-07-03 13:41:52 -0300

    Starter Config

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+import os
+
+# ----- Constantes e configurações iniciais -----
+LARGURA_TELA = 800
+ALTURA_TELA = 600
+TITULO_JOGO = "Coletor Cósmico"
+COR_PRETO = (0, 0, 0)
+
+pygame.init()
+pygame.mixer.init()
